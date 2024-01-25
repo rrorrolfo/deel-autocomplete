248f39a5272dd527a14451e82d4d8262d0cb97c7
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-25 15:06:06 +0200

    Added and styled title text

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Autocomplete from "./features/autocomplete";
 function App() {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Deel Autocomplete</h1>
       <Autocomplete />
     </div>
   );
