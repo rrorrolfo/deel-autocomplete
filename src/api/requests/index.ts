233d5a5b12a03cd271d97a33d83faec3339c14ff
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-25 11:23:25 +0200

    Created fetchProductsByName request

---
+++ src/api/requests/index.ts
@@ -0,0 +1,18 @@
+export const fetchProductsByName = (name: string) =>
+  fetch(`https://api.escuelajs.co/api/v1/products/?title=${name}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network error");
+      }
+      return res.json();
+    })
+    .then((data) => data)
+    .catch((error) => {
+      if (error instanceof Error) {
+        console.error(
+          "There was an error while fetching products by name:",
+          error.message
+        );
+      }
+      throw error;
+    });
