4ea9027f4983f30a3ba58e57f182a187964c274a
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-25 17:48:23 +0200

    Wrapped fetchProductsByName inside try catch block for error handling

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,19 +2,25 @@ import { useEffect, useState } from "react";
 import { ProductType } from "../common/types/products";
 import { fetchProductsByName } from "../api/requests";
 
-export const useFetchProductsByName = (
-  value: string
-): { products: ProductType[] } => {
+type UseFetchReturnType = { products: ProductType[]; errorFetching: boolean };
+
+export const useFetchProductsByName = (value: string): UseFetchReturnType => {
   const [products, setproducts] = useState<ProductType[]>([]);
+  const [errorFetching, toggleErrorFetching] = useState(false);
 
   useEffect(() => {
     const debounceFetchProducts = setTimeout(async () => {
-      const products: ProductType[] = await fetchProductsByName(value);
-      setproducts(products);
+      try {
+        const products: ProductType[] = await fetchProductsByName(value);
+        setproducts(products);
+        toggleErrorFetching(false);
+      } catch (e) {
+        toggleErrorFetching(true);
+      }
     }, 300);
 
     return () => clearTimeout(debounceFetchProducts);
   }, [value]);
 
-  return { products };
+  return { products, errorFetching };
 };
