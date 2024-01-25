ce60dc623bb63468ad3c7156ef9931f7287b8035
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-25 12:39:13 +0200

    Added functionality to highlight title from fetched products

--- src/features/autocomplete/index.tsx
+++ src/features/autocomplete/index.tsx
@@ -2,11 +2,17 @@ import { useState, useRef, useEffect } from "react";
 import styles from "./autocomplete.module.css";
 import SuggestionsTable from "./suggestionsTable";
 import { fetchProductsByName } from "../../api/requests";
-import { ProductType } from "../../common/types/products";
+import {
+  ProductType,
+  ProductWithHiglightTitle,
+} from "../../common/types/products";
+import { getHighlightedTitle } from "../../common/utils";
 
 const Autocomplete = () => {
   const [value, setValue] = useState("");
-  const [suggestions, setSuggestions] = useState<ProductType[]>([]);
+  const [suggestions, setSuggestions] = useState<ProductWithHiglightTitle[]>(
+    []
+  );
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
@@ -19,7 +25,15 @@ const Autocomplete = () => {
   useEffect(() => {
     const debounceFetchProducts = setTimeout(async () => {
       const products: ProductType[] = await fetchProductsByName(value);
-      setSuggestions(products);
+
+      const highlightedProducts: ProductWithHiglightTitle[] = products.map(
+        (product) => ({
+          ...product,
+          highlightTitle: getHighlightedTitle(product, value),
+        })
+      );
+
+      setSuggestions(highlightedProducts);
     }, 300);
 
     return () => clearTimeout(debounceFetchProducts);
