f1f0ead94c91233533f9a6063c19e430617e1305
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-25 11:27:51 +0200

    Created ProductType interface

---
+++ src/common/types/products.ts
@@ -0,0 +1,16 @@
+export interface ProductType {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: Date;
+  updatedAt: Date;
+  category: {
+    id: number;
+    name: string;
+    image: string;
+    creationAt: Date;
+    updatedAt: Date;
+  };
+}
