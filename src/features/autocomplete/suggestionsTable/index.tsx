dc042ec15a42cd51c6ae2d616b5c0163bc7b3301
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-26 03:38:50 +0200

    Updated SuggestionsTable component to use useFetchProductsByName hook and handle suggestions by itself

--- src/features/autocomplete/suggestionsTable/index.tsx
+++ src/features/autocomplete/suggestionsTable/index.tsx
@@ -1,23 +1,57 @@
-import { ProductWithHiglightTitle } from "../../../common/types/products";
+import { useMemo } from "react";
 import styles from "./suggestionsTable.module.css";
+import { ProductWithHiglightTitle } from "../../../common/types/products";
+import { useFetchProductsByName } from "../../../hooks";
+import { getHighlightedTitle } from "../../../common/utils";
 
 type SuggestionsTableProps = {
-  suggestions: ProductWithHiglightTitle[];
+  debouncedValue: string;
+  showSuggestions: boolean;
 };
 
-const SuggestionsTable = ({ suggestions }: SuggestionsTableProps) => {
+const SuggestionsTable = ({
+  debouncedValue,
+  showSuggestions,
+}: SuggestionsTableProps) => {
+  const { products, errorFetching } = useFetchProductsByName(debouncedValue);
+
+  const highlightedProducts: ProductWithHiglightTitle[] = useMemo(
+    () =>
+      products.slice(0, 10).map((product) => ({
+        ...product,
+        highlightTitle: getHighlightedTitle(product, debouncedValue),
+      })),
+    [products, debouncedValue]
+  );
+
+  if (errorFetching) {
+    return (
+      <span className={styles.errorMessage}>
+        There was an error, please search for other product name
+      </span>
+    );
+  }
+
+  if (debouncedValue === "") {
+    return <></>;
+  }
+
   return (
-    <div className={styles.resultsTable}>
-      <ul className={styles.suggestionsList}>
-        {suggestions.length ? (
-          suggestions.map(({ highlightTitle, id }) => (
-            <li key={id}>{highlightTitle}</li>
-          ))
-        ) : (
-          <li>No suggestions match your search</li>
-        )}
-      </ul>
-    </div>
+    <>
+      {showSuggestions && (
+        <div className={styles.resultsTable}>
+          <ul className={styles.suggestionsList}>
+            {highlightedProducts.length ? (
+              highlightedProducts.map(({ highlightTitle, id }) => (
+                <li key={id}>{highlightTitle}</li>
+              ))
+            ) : (
+              <li>No suggestions match your search</li>
+            )}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 export default SuggestionsTable;
